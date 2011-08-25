3addd85ab3aa366d35e181a5760b688e7a2481ac
EdGruberman <ed@rjump.com>
2011-08-25 11:44:04 -0700

    CHG: Removed extraneous code

--- src/edgruberman/bukkit/wrappercommands/Main.java
+++ src/edgruberman/bukkit/wrappercommands/Main.java
@@ -4,13 +4,9 @@ import edgruberman.bukkit.messagemanager.MessageManager;
 
 public final class Main extends org.bukkit.plugin.java.JavaPlugin {
     
-    static ConfigurationFile configurationFile;
     static MessageManager messageManager;
     
     public void onLoad() {
-        Main.configurationFile = new ConfigurationFile(this);
-        Main.configurationFile.load();
-        
         Main.messageManager = new MessageManager(this);
         Main.messageManager.log("Version " + this.getDescription().getVersion());
     }
